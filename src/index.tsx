3be5eedbff68c5d88ed0fb343fe9d9b1a05aab10
Yogesh3141 <102430650+Yogesh3141@users.noreply.github.com>
2022-12-19 13:40:36 +0530

    #changes

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,8 @@ mergeStyles({
   },
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
